b07044f8e16d53921fae977d3c8ccec75951f578
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-06-03 11:19:54 +0530

    created server and started with file system

---
+++ data.js
@@ -0,0 +1,9 @@
+const data=[
+    {name:'Abhilash',email:'abhi@test.com'},
+    {name:'Sam',email:'sam@test.com'},
+    {name:'Peter',email:'peter@test.com'},
+    {name:'Yoyo',email:'yoy@test.com'},
+    {name:'yaya',email:'aya@test.com'},
+]
+
+module.exports=data;
