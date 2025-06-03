3b7a81a622a5621f3cbb74ea12ac9c7660c32e03
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-06-03 16:35:50 +0530

    Query params

--- index.js
+++ index.js
@@ -83,7 +83,9 @@ const express=require('express')
 const app=express();
 
 app.get('',(req,res)=>{
+    console.log("Data sent by browser",req.query.name)
     res.send("Hello,this is home page")
+    
 })
 app.get('/about',(req,res)=>{
     res.send("Hello,this is about page")
