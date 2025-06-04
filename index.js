241fcf812f010cea747d26630b42dc9637877ee7
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-06-04 14:29:27 +0530

    ROute level middleware

--- index.js
+++ index.js
@@ -116,25 +116,28 @@ fs.rename(filePath,`${dirPath}/testing.txt`,(err)=>{
 
 const express=require('express');
 const app=express();
+const reqFilter=require('./middleware')
+const route=express.Router()
 
-const reqFilter=(req,resp,next)=>{
-    if(!req.query.age){
-        resp.send('Please provide age')
-    }
-    else if(req.query.age<18){
-        resp.send('You can not access this page')
-    }
-    else{
- next();
-    }
-   
-}
-app.use(reqFilter)
+
+route.use(reqFilter)
+
+// Application level middleware 
+// app.use(reqFilter)
 
 app.get('/',(req,resp)=>{
     resp.send('Welcome to home page')
 })
+//Route level middleware
 app.get('/users',(req,resp)=>{
     resp.send('Welcome to users  page')
 })
+route.get('/about',(req,resp)=>{
+    resp.send('Welcome to about page')
+})
+route.get('/contact',(req,resp)=>{
+    resp.send('Welcome to contact page')
+})
+app.use('/',route)
+
 app.listen(5000);
