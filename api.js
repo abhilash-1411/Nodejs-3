73b23771960388e0c26ab6eef0a29fdcf49f6c32
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-06-26 14:36:44 +0530

    POST api

--- api.js
+++ api.js
@@ -2,11 +2,17 @@ const express=require('express');
 const dbConnect=require('./mongodb');
 const app=express();
 
+app.use(express.json())
 app.get('/',async(req,resp)=>{
     let data= await dbConnect();
      data=await data.find().toArray();
-     console.log(data);
     resp.send(data)
-  
+})
+
+app.post('/',async(req,resp)=>{
+    let data= await dbConnect()
+    let result= await data.insertOne(req.body)
+    // console.log(result)
+    resp.send(result)
 })
 app.listen(5000);
