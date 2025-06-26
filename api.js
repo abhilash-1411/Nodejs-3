35e524efcde5feec3082808dbec61b57f6deb80c
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-06-26 18:54:38 +0530

    Delete API

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const dbConnect=require('./mongodb');
 const app=express();
+const mongodb=require('mongodb')
 
 app.use(express.json())
 app.get('/',async(req,resp)=>{
@@ -20,5 +21,11 @@ app.put('/:name',async(req,resp)=>{
     let data=await dbConnect();
     let res=await data.updateOne({name:req.params.name}, {$set:req.body})
     resp.send(res)
+})
+app.delete('/:id',async(req,resp)=>{
+    let data= await dbConnect()
+    let res=await data.deleteOne({_id:new mongodb.ObjectId(req.params.id)})
+    resp.send(res);
+
 })
 app.listen(5000);
