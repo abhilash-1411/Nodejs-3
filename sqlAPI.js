541e7d80bfb5c031c329ccd21272738edf7daa40
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-03 18:20:48 +0530

    POST API using mysql

--- sqlAPI.js
+++ sqlAPI.js
@@ -2,6 +2,7 @@ const express=require('express');
 const con=require('./config')
 
 const app=express();
+app.use(express.json());
 
 app.get('/',(req,resp)=>{
     con.query("select * from users",(err,result)=>{
@@ -13,6 +14,14 @@ app.get('/',(req,resp)=>{
         }
     })
     
+})
+app.post('/',(req,resp)=>{
+    // const data={name:'Ankit',age:'23',category:'Player',brand:'YYY'}
+    const data=req.body
+    con.query('INSERT into users SET ?',data,(error,result,fields)=>{
+     if(error) error ;
+     resp.send(result)
+  })
 })
 app.listen(5000,()=>{
     console.log("Server is listening on PORT 5000")
